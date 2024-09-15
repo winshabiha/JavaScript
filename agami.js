605a5aad717940ea70c3bc5cb94e50afb13bbe50
winshabiha <sabiha3289@gmail.com>
2024-09-15 23:59:14 +0600

    another calculation added

---
+++ agami.js
@@ -0,0 +1,40 @@
+var person1 = "Sabiha"
+var person2 = "Jahan"
+// console.log(person1 + person2)
+// console.log(person1 + ' ' + person2)
+
+var price = '500';
+// console.log(typeof price)
+// console.log(parseInt (price))
+
+var price = '800';
+var fruits = 400;
+var vegetables = '300';
+var changedVegetable = parseInt(vegetables)
+// console.log(fruits + vegetables)
+// console.log(fruits + changedVegetable)
+
+// -, *, /
+// console.log(price - fruits)
+// console.log(price * fruits)
+// console.log(price / fruits)
+
+// var price = '99.99999'
+// console.log(parseFloat (price))
+// console.log(parseInt (price))
+
+// var value;
+// console.log(value)
+
+var price = 1685;
+// price = 1000;
+// console.log(price)
+// price = price + 500
+// console.log(price)
+var newprice = 400;
+// console.log(price + newprice)
+// price +=15;
+// console.log(price)
+
+
+
---
+++ comparison.js
@@ -0,0 +1,27 @@
+// console.log(3>2)
+// console.log(7<5)
+// console.log(99 == 99)
+// console.log(99 == 199)
+// console.log(99 != 199)
+// console.log(99 == '199')
+// console.log(99 === '199')
+// console.log(99 == '99')
+// console.log(99 === '99')
+
+
+// console.log(9>5 && 5>9)
+// console.log(9>5 && 5>9 && 4>1)
+// console.log(9>5 && 5<9 && 4>1)
+
+
+// console.log(9>5 || 5>9 )
+// console.log(9>5 || 5>9 || 5>10 )
+// console.log(4<3 || 5>9 || 15<8 )
+
+var AjkBrishtiHobeKina = 'AjkBrishtiHobe'
+if (AjkBrishtiHobeKina === 'AjkBrishtiHobe'){
+    console.log('AmiTuitioneJaboNa')
+}
+else{
+    console.log('AmiTuitioneJabo')
+}
